3a1c04fbcdbdc0fb989ae35cc2f63d74e4ed9093
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2025-08-13 09:24:33 -0500

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Gastos Robert</h1>
+      <p>¡Bienvenido a la aplicación!</p>
+    </div>
+  );
+}
+
+export default App;
