ac051a1fece3a8ebdf0b5ca8982f92d3f1d7215a
Robert Niño <robertfenyiner@hotmail.com>
2026-03-15 01:27:32 +0000

    Fix frontend entrypoints and Tailwind build

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      <h1>Gastos Robert</h1>
-      <p>¡Bienvenido a la aplicación!</p>
-    </div>
-  );
-}
-
-export default App;
+export { default } from './App.tsx';
